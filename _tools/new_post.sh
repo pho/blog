d9c6fa2003869063aca4552db4467b64d5939c9d
pho (nilfheim) <pho@phofe.es>
2011-08-02 22:20:04 +0200

    create/edit existing posts

--- _tools/new_post.sh
+++ _tools/new_post.sh
@@ -1,5 +1,10 @@
 echo -n "Title: "
 read title
-filename=_`date +%Y-%m-%d`-`python -c "print '-'.join('$title'.split(' '))"`.textile
-editor _posts/$filename
-echo "Saved as $filename. Remove the underscore for publish"
+filename=`date +%Y-%m-%d`-`python -c "print '-'.join('$title'.split(' '))"`.textile
+if [[ -f _posts/$filename ]]; then
+	editor _posts/$filename
+else
+	editor _posts/_$filename
+	echo "Saved draft as _$filename. Remove the underscore for publish"
+	echo "REMEMBER: Add it to the git tree if you want to edit the file in another computer"
+fi
