ab395be0d7cf3cc300acae5672b1af78ec323558
pho (nilfheim) <pho@phofe.es>
2011-08-02 22:58:39 +0200

    Added sample layout and title

--- _tools/new_post.sh
+++ _tools/new_post.sh
@@ -4,6 +4,10 @@ filename=`date +%Y-%m-%d`-`python -c "print '-'.join('$title'.split(' '))"`.text
 if [[ -f _posts/$filename ]]; then
 	editor _posts/$filename
 else
+	echo "---" >> _posts/_$filename
+	echo "layout: default" >> _posts/_$filename
+	echo "title: $title" >> _posts/_$filename
+	echo "---" >> _posts/_$filename
 	editor _posts/_$filename
 	echo "Saved draft as _$filename. Remove the underscore for publish"
 	echo "REMEMBER: Add it to the git tree if you want to edit the file in another computer"
