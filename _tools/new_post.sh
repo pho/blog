3ba83a4e97bec00d3df565156867f1cccd440b34
pho <pho@phofe.es>
2011-10-26 21:30:41 +0200

    Update on new_post script, now supports python3

--- _tools/new_post.sh
+++ _tools/new_post.sh
@@ -1,6 +1,6 @@
 echo -n "Title: "
 read title
-filename=`date +%Y-%m-%d`-`python -c "print '-'.join('$title'.split(' '))"`.textile
+filename=`date +%Y-%m-%d`-`python -c "print('-'.join('$title'.split(' ')))"`.textile
 if [[ -f _posts/$filename ]]; then
 	editor _posts/$filename
 else
@@ -8,7 +8,7 @@ else
 	echo "layout: post" >> _posts/_$filename
 	echo "title: $title" >> _posts/_$filename
 	echo "---" >> _posts/_$filename
-	editor _posts/_$filename
+	edit _posts/_$filename
 	echo "Saved draft as _$filename. Remove the underscore for publish"
 	echo "REMEMBER: Add it to the git tree if you want to edit the file in another computer"
 fi
